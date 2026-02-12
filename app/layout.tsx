92a12108a22805bd4da871a197007df82223f1bd
mino <mino@localhost>
2026-02-11 23:51:29 -0800

    remove plausible
    should've been removed ages ago. I don't want analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Providers from "@/components/Providers";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import Navbar from "@/components/nav/Navbar";
-import PlausibleProvider from "next-plausible";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -97,21 +96,12 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-200`}
       >
-        <PlausibleProvider 
-        domain="oppai-daisuki.net"
-        customDomain="stats.oppai-daisuki.net"
-        trackOutboundLinks={true}
-        trackFileDownloads={true}
-        selfHosted={true}
-        >
         <ThemeProvider>
           <Providers>
             <Navbar />
             {children}
           </Providers>
         </ThemeProvider>
-
-        </PlausibleProvider>
       </body>
     </html>
   );
